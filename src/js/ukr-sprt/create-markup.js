a0260a4829d16df903c376a7a653d0251d87946e
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-07-08 01:25:53 +0300

    fixed display markup

--- src/js/ukr-sprt/create-markup.js
+++ src/js/ukr-sprt/create-markup.js
@@ -9,19 +9,19 @@ sliderRef.insertAdjacentHTML('beforeend', markup);
 
 function createMarkup(slide, index){
     return `<div class="swiper-slide">
-      <div class="slide__content">
-        <div class="slide__number">${(index + 1)
-          .toString()
-          .padStart(2, '0')}</div>
-        <a href="${slide.url}" class="slider__link" target="blank">
-          <img srcset="${slide.img.imageUrl}, ${
-    slide.img.imageUrl2x
-  } 2x" 
-          src="${slide.img.imageUrl}"
-          alt="${slide.title}" 
-          class="slider__png">
-        </a>
-      </div>
-    </div>`;
+    <div class="slide__content">
+      <div class="slide__number">${(index + 1)
+        .toString()
+        .padStart(2, '0')}</div>
+      <a href="${slide.url}" class="slider__link" target="blank">
+        <img srcset="${slide.img.imageUrl.href}, ${
+  slide.img.retinaImageUrl.href
+} 2x" 
+        src="${slide.img.imageUrl.href}"
+        alt="${slide.title}" 
+        class="slider__png">
+      </a>
+    </div>
+  </div>`;
 }
 
