ccc4d575c58b8b99e071f1f7ad45d486fa78dd6c
lex <savchenko.lex@gmail.com>
2023-07-10 12:13:16 +0300

    fix index js resolve problem

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import './js/ukr-sprt/ukr-sprt-slider';
 import './js/best_sellers_load';
 import './js/category-list';
 import './js/all-books-by-category';
-import './js/add-to-shopping-list';
+// import './js/add-to-shopping-list';
 import './js/theme-switcher';
-import './js/all-books-by-seemore/fetch-books-by-category-api';
-import './js/all-books-by-seemore/all-books-by-category';
-<<<<<<< HEAD
+// import './js/all-books-by-seemore/fetch-books-by-category-api';
+// import './js/all-books-by-seemore/all-books-by-category';
 import './js/shopping-list';
-=======
-import './js/add-to-shopping-list';
 import './js/theme-switcher'
 import './js/modal'
->>>>>>> origin/modal/open-close-js
