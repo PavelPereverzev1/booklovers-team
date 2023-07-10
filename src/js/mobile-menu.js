7f0a9971f23c6c0ec5861878defc40cb0d2f0a52
yurii-corssa <yurii.corssa@gmail.com>
2023-07-10 19:38:20 +0200

    open-close mobile menu

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,61 @@
+import { save, load } from './storage';
+
+const refs = {
+  burgerBtn: document.querySelector('.burger-btn'),
+  mobileMenuContainer: document.querySelector('.mobile-menu_container'),
+  mobileMenuGuest: document.querySelector('.mobile-menu_guest'),
+  mobileMenuUser: document.querySelector('.mobile-menu_user'),
+  burgerIcon: document.querySelector('.burger-ico'),
+};
+
+refs.burgerBtn.addEventListener('click', handleMobileMenu);
+
+const user = {
+  name: 'userName',
+};
+let menuStatus = 'close';
+
+save('user', user);
+
+function handleMobileMenu() {
+  if (menuStatus === 'close') {
+    console.log('close');
+    openMobileMenu();
+    menuStatus = 'open';
+    return;
+  }
+  if (menuStatus === 'open') {
+    console.log('open');
+    closeMobileMenu();
+    menuStatus = 'close';
+    return;
+  }
+}
+
+function openMobileMenu() {
+  if (load('user')) {
+    removeClass(refs.mobileMenuContainer, 'visually-hidden');
+    addClass(refs.mobileMenuUser, 'open');
+    return;
+  }
+  removeClass(refs.mobileMenuContainer, 'visually-hidden');
+  addClass(refs.mobileMenuGuest, 'open');
+}
+
+function closeMobileMenu() {
+  if (load('user')) {
+    addClass(refs.mobileMenuContainer, 'visually-hidden');
+    removeClass(refs.mobileMenuUser, 'open');
+    return;
+  }
+  addClass(refs.mobileMenuContainer, 'visually-hidden');
+  removeClass(refs.mobileMenuGuest, 'open');
+}
+
+function addClass(object, value) {
+  object.classList.add(value);
+}
+
+function removeClass(object, value) {
+  object.classList.remove(value);
+}
