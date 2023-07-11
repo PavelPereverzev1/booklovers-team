fd9680e903cf1c6afa6090d20bb247e106543cb8
lex <savchenko.lex@gmail.com>
2023-07-11 08:32:16 +0300

    minor changes

--- src/js/category-list.js
+++ src/js/category-list.js
@@ -11,22 +11,22 @@ async function loadHomepage(){
 }
 loadHomepage();
 
-function hideBook() {
-  let screenSize = window.innerWidth;
-  const element = document.querySelectorAll('js_genre_cards_list');
-  console.log('current window size is ' + screenSize + 'px');
-  console.log('Best Sellers Books array:');
-  console.dir(element);
-  if (screenSize <= 768) {
-    console.dir(element.childNodes);
-    // const length = element.childNodes.length - 1;
-    // console.log(Number(length));
-  } else {
-    // element.classList.remove('visually-hidden');
-    // console.dir(element);
-  }
-}
+// function hideBook() {
+//   let screenSize = window.innerWidth;
+//   const element = document.querySelectorAll('js_genre_cards_list');
+//   console.log('current window size is ' + screenSize + 'px');
+//   console.log('Best Sellers Books array:');
+//   console.dir(element);
+//   if (screenSize <= 768) {
+//     console.dir(element.childNodes);
+//     // const length = element.childNodes.length - 1;
+//     // console.log(Number(length));
+//   } else {
+//     // element.classList.remove('visually-hidden');
+//     // console.dir(element);
+//   }
+// }
 
-hideBook();
+// hideBook();
 
-window.addEventListener('resize', hideBook);
+// window.addEventListener('resize', hideBook);
